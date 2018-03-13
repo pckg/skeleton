6eb1fd15c845b156e6f884174c9e6e19aa916e65
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-03-13 19:23:57 +0100

    Framework changes

--- www/dev.php
+++ www/dev.php
@@ -3,5 +3,8 @@
 /**
  * Simply require development entry point, framework will take care of things.
  */
-define('__ROOT__', dirname(dirname(__FILE__)));
-require_once __ROOT__ . "/vendor/pckg/framework/src/development.php";
+define('__ROOT__', isset($_SERVER['DOCUMENT_ROOT'])
+    ? $_SERVER['DOCUMENT_ROOT']
+    : (dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR)
+);
+require_once __ROOT__ . "vendor/pckg/framework/src/development.php";
--- www/index.php
+++ www/index.php
@@ -3,5 +3,8 @@
 /**
  * Simply require production entry point, framework will take care of things.
  */
-define('__ROOT__', dirname(dirname(__FILE__)));
-require_once __ROOT__ . "/vendor/pckg/framework/src/production.php";
+define('__ROOT__', isset($_SERVER['DOCUMENT_ROOT'])
+    ? $_SERVER['DOCUMENT_ROOT']
+    : (dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR)
+);
+require_once __ROOT__ . "vendor/pckg/framework/src/production.php";
