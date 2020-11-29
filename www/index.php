15039226949ea7617e4c333645049a22e7376629
Bojan Rajh <schtr4jh@schtr4jh.net>
2020-11-29 15:56:46 +0100

    Update packages

--- www/index.php
+++ www/index.php
@@ -4,7 +4,8 @@
  * Simply require production entry point, framework will take care of things.
  */
 define('__ROOT__', isset($_SERVER['DOCUMENT_ROOT'])
-    ? $_SERVER['DOCUMENT_ROOT']
+    ? preg_replace('#www/$#', '', rtrim($_SERVER['DOCUMENT_ROOT'], DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR)
     : (dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR)
 );
+
 require_once __ROOT__ . "vendor/pckg/framework/src/production.php";
